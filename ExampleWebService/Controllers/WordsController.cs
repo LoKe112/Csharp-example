ab2158e9c4c7d91e5f64d9bd827ab63127abdee7
LoKe112 <128743847+LoKe112@users.noreply.github.com>
2025-10-27 17:21:28 +0400

    Update WordsController.cs

--- ExampleWebService/Controllers/WordsController.cs
+++ ExampleWebService/Controllers/WordsController.cs
@@ -13,7 +13,7 @@ namespace ExampleWebService.Controllers;
 public class WordsController(IWordRepository wordRepository) : ControllerBase
 {
     /// <summary>
-    /// Retrieves the list of all stored words Lol.
+    /// Retrieves the list of all stored words xd.
     /// </summary>
     /// <returns>
     /// A JSON array containing all words.
